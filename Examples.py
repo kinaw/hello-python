2c1ee8e94d9b24654e68c30c2c082899ce24cdd9
git-name <git-email>
2018-10-26 00:57:27 +0800

    sad

---
+++ Examples.py
@@ -0,0 +1,6 @@
+x = 1
+while x < 100:
+    if x + 2 == 1:
+     print(x, end="")
+     x += 2
+print()
---
+++ Quinao_e3.py
@@ -0,0 +1,10 @@
+input = input("[Enter a comma separated list of numbers]:")
+
+#print("Your Input Number {}")
+
+x = 0
+while x < 4:
+    #if x ** 2:
+    print("[Sum of Squares]:")
+    x += 1
+    
